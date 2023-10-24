a881089d6de79913b33d067fb5a676faa2a2a7d3
Samuli Lehtinen <slehtinen2000@gmail.com>
2023-10-24 16:48:07 +0200

    reset_device class complete

--- static_scheduling/reset_device.cpp
+++ static_scheduling/reset_device.cpp
@@ -1,11 +1,40 @@
 // includes come here
 #include "reset_device.hpp"
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 
 #if defined(TARGET_DISCO_H747I)
 #define PUSH_BUTTON BUTTON1
 static constexpr uint8_t kPolarityPressed = 1;
 #endif
 
+namespace static_scheduling{
 
-// code comes here
+ResetDevice::ResetDevice(Timer& timer) : _timer(timer), _resetButton(BUTTON1) {}
+
+// definition of task execution time
+static constexpr std::chrono::microseconds kTaskRunTime = 100000us;
+
+bool ResetDevice::checkReset() {
+    std::chrono::microseconds initialTime = _timer.elapsed_time();
+    std::chrono::microseconds elapsedTime = std::chrono::microseconds::zero();
+    int buttonState = 0;
+    while(elapsedTime < kTaskRunTime){
+            buttonState = _resetButton.read();  // Read the actual button state
+    }
+    return (buttonState == kPolarityPressed); 
+
+}
+
+void static_scheduling::ResetDevice::onRise() {
+
+    _pressTime = _timer.elapsed_time();
+
+}
+
+std::chrono::microseconds static_scheduling::ResetDevice::getPressTime() {
+    return _pressTime;
+}
+}
